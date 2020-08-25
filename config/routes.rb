e71d2500f62635ac34a2a305dcb544848acf4f21
fbado66 <fbado66@gmail.com>
2020-08-25 18:37:42 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+ 
+
+
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres
+  resources :songs
+
+  # get 'artists', to: 'artists#index'
+
+  get 'artists/:id', to: 'artists#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
